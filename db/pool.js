d56408e8626163f53d0bd43bd742832a780623cf
Callum Preston <CallumCPreston@outlook.com>
2025-08-27 12:48:11 +0100

    Debugging deployment

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,8 @@
 const { Pool } = require("pg");
-
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 //Create a new pool to be used... Note that this is able to be done as
 //the .env file is configured in app.js before this is created
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 })
